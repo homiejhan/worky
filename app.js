2c84830844c7f357023f78bca0bbbea84655e758
homiejhan <justhan47@gmail.com>
2026-05-31 06:09:23 -0500

    Add two new colors to CAL_COLORS array

--- app.js
+++ app.js
@@ -1085,7 +1085,7 @@ tickAll();
 
 const CAL_HOUR_PX   = 64;
 const CAL_DAY_COUNT = 7;
-const CAL_COLORS    = ['#378ADD','#EC3636','#8B5CF6','#F97316','#22C55E','#EAB308','#5DCAA5','#D4537E'];
+const CAL_COLORS = ['#378ADD','#EC3636','#8B5CF6','#F97316','#22C55E','#EAB308','#5DCAA5','#D4537E','#505050','#FFF5EE'];
 const CAL_LS_KEY    = 'focus-cal-state';
 const CAL_DOW       = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 const CAL_TOTAL_PX  = 24 * CAL_HOUR_PX;
